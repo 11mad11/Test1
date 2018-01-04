456a69b870aca731212923fe045f630837094c70
21mad21 <mad.21@live.fr>
2018-01-04 17:31:06 -0500

    initial commit

---
+++ desktop/src/fr/mad/test1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.mad.test1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.mad.test1.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
